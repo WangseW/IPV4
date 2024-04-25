98f32f6d8954b1034dced8935fc13ae9812ddcbd
南郇传媒 <146552686+WangseW@users.noreply.github.com>
2024-04-25 14:41:51 +0800

    Update wspd.py

--- wspd.py
+++ wspd.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     date = today.strftime("%m%d")
     
     channels = [
-        "北京卫视:bjws","天津卫视:tjws","东方卫视:dfws","重庆卫视:cqws","黑龙江卫视:hljws","辽宁卫视:lnws","河北卫视:hbws","山东卫视:sdws","安徽卫视:ahws","湖北卫视:hubws","湖南卫视:hunws","江西卫视:jxws","江苏卫视:jsws","浙江卫视:zjws","东南卫视:dnws","广东卫视:gdws","深圳卫视:szws","广西卫视:gxws","贵州卫视:gzws","四川卫视:scws","新疆卫视:xjws","海南卫视:hinws","兵团卫视:btws","河南卫视:hnws"
+        "北京卫视:bjws","山西卫视:sxws","天津卫视:tjws","东方卫视:dfws","重庆卫视:cqws","黑龙江卫视:hljws","辽宁卫视:lnws","河北卫视:hbws","山东卫视:sdws","安徽卫视:ahws","湖北卫视:hubws","湖南卫视:hunws","江西卫视:jxws","江苏卫视:jsws","浙江卫视:zjws","东南卫视:dnws","广东卫视:gdws","深圳卫视:szws","广西卫视:gxws","贵州卫视:gzws","四川卫视:scws","新疆卫视:xjws","海南卫视:hinws","兵团卫视:btws","河南卫视:hnws"
  ]
  
 with open("./直播/wspd.txt", "w") as f:
